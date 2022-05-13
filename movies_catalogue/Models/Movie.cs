c2f56ba5688bda4c0240e5b02684ed984c4b6f7a
Ana Veljanoska <anaveljanoska@yahoo.com>
2022-05-13 20:00:24 +0200

    frontend movies index

--- movies_catalogue/Models/Movie.cs
+++ movies_catalogue/Models/Movie.cs
@@ -8,11 +8,22 @@ namespace movies_catalogue.Models
         [Key]
         public int ID { get; set; }
         [Required]
-        [Display(Name ="Name")]
+        [Display(Name = "Name")]
         public string MovieName { get; set; }
         public string ImdbLink { get; set; }
         public string PictureURL { get; set; }
         public DateTime ReleaseDate { get; set; }
         public double Rating { get; set; }
+
+        public string TimeLeft()
+        {
+            TimeSpan timespan = ReleaseDate.Subtract(DateTime.Now);
+            double isReleased = timespan.TotalSeconds;
+
+            if (isReleased < 0)
+                return "Already released!";
+            else
+                return timespan.ToString(@"d\.h\:mm\:ss") + " time left until release!";
+        }
     }
 }
